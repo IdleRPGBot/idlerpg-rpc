6ea0e667adb3996393068596caac62b9c296296b
Jens Reidel <adrian@travitia.xyz>
2021-07-04 22:02:08 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
-use discord_rpc_client::{models::Activity, Client};
+use discord_rich_presence::{activity, new_client, DiscordIpc};
+use serde_json::json;
 
 use std::{thread::sleep, time::Duration};
 
 mod profile;
 
-const CLIENT_ID: u64 = 424606447867789312;
+const CLIENT_ID: &str = "424606447867789312";
 const REFRESH_INTERVAL: Duration = Duration::from_millis(5000);
 pub static mut USER_ID: u64 = 0;
 
-fn set_updated_activity(activity: Activity) -> Activity {
-    let profile_data = profile::get_profile();
-    let mut act = activity
-        .state(&profile_data.get_state())
-        .details(&profile_data.get_details())
-        .assets(|ass| {
-            ass.large_image("logo")
-                .large_text(profile_data.get_big_image_text())
-                .small_image(profile_data.get_small_image())
-                .small_text(&profile_data.get_small_image_text())
-        });
-    if let Some(ts) = profile_data.get_time() {
-        act = act.timestamps(|_| ts)
-    }
-    act
-}
-
 fn main() {
-    let mut discord = Client::new(CLIENT_ID);
-    discord.start();
-    unsafe {
-        USER_ID = discord
-            .ready_data()
-            .unwrap()
-            .get_user()
-            .unwrap()
-            .get_id()
-            .unwrap()
-            .parse()
-            .unwrap()
-    };
+    let mut discord = new_client(CLIENT_ID).unwrap();
+    println!("Connecting to Discord");
+    discord.connect_ipc().unwrap();
+    println!("Connected, sending handshake");
+    discord
+        .send(
+            json!({
+                "v": 1,
+                "client_id": discord.get_client_id()
+            }),
+            0,
+        )
+        .unwrap();
+    let (_, data) = discord.recv().unwrap();
+    let id = data["data"]["user"]["id"]
+        .as_str()
+        .unwrap()
+        .to_owned()
+        .parse()
+        .unwrap();
 
-    sleep(REFRESH_INTERVAL);
+    unsafe {
+        USER_ID = id;
+    }
 
     loop {
-        sleep(REFRESH_INTERVAL);
-
         if unsafe { USER_ID } != 0 {
-            let _ = discord.set_activity(set_updated_activity);
+            let profile_data = profile::get_profile();
+            let state = profile_data.get_state();
+            let details = profile_data.get_details();
+            let small_text = profile_data.get_small_image_text();
+            let mut act = activity::Activity::new()
+                .state(&state)
+                .details(&details)
+                .assets(
+                    activity::Assets::new()
+                        .large_image("logo")
+                        .large_text(profile_data.get_big_image_text())
+                        .small_image(profile_data.get_small_image())
+                        .small_text(&small_text),
+                );
+            if let Some(ts) = profile_data.get_time() {
+                act = act.timestamps(ts)
+            }
+            let _ = discord.set_activity(act);
         }
+
+        sleep(REFRESH_INTERVAL);
     }
 }
--- src/profile.rs
+++ src/profile.rs
@@ -1,28 +1,28 @@
 use crate::USER_ID;
 
-use discord_rpc_client::models::ActivityTimestamps;
-use nanoserde::DeJson;
-use once_cell::unsync::Lazy;
+use discord_rich_presence::activity::Timestamps;
+use once_cell::sync::Lazy;
+use serde::Deserialize;
 use std::time::{SystemTime, UNIX_EPOCH};
 use ureq::Agent;
 use url::Url;
 
-const AGENT: Lazy<Agent> = Lazy::new(|| Agent::new());
-const URL: Lazy<Url> = Lazy::new(|| {
+static AGENT: Lazy<Agent> = Lazy::new(Agent::new);
+static URL: Lazy<Url> = Lazy::new(|| {
     Url::parse(&format!("https://api.idlerpg.xyz/user?id={}", unsafe {
         USER_ID
     }))
     .unwrap()
 });
 
-#[derive(DeJson)]
+#[derive(Deserialize)]
 pub struct AdventureData {
     pub done: bool,
     pub time_left: i64,
     pub number: u8,
 }
 
-#[derive(DeJson)]
+#[derive(Deserialize)]
 pub struct ProfileData {
     pub character_name: String,
     pub level: u8,
@@ -44,15 +44,15 @@ impl ProfileData {
             None => String::from("Idling"),
         }
     }
-    pub fn get_time(&self) -> Option<ActivityTimestamps> {
+    pub fn get_time(&self) -> Option<Timestamps> {
         if let Some(adv) = &self.adventure {
             if adv.time_left > 0 {
                 let now = SystemTime::now()
                     .duration_since(UNIX_EPOCH)
                     .unwrap()
-                    .as_secs() as u64;
-                let then = now + (adv.time_left as u64);
-                let ts = ActivityTimestamps::new().start(now).end(then);
+                    .as_secs() as i32;
+                let then = now + (adv.time_left as i32);
+                let ts = Timestamps::new().start(now).end(then);
                 Some(ts)
             } else {
                 None
@@ -82,7 +82,7 @@ pub fn get_profile() -> ProfileData {
         .unwrap()
         .into_string()
         .unwrap();
-    ProfileData::deserialize_json(&body).unwrap()
+    serde_json::from_str(&body).unwrap()
 }
 
 pub fn get_class_image(class: &str) -> &str {
